3b007cfe820efe2882771627c0167b280d4f8e98
Aida <Aida@MacBook-Air.local>
2022-09-03 10:08:27 +0600

    Написать программу вычисления произведения чисел от 1 до N

---
+++ 4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+void Multiplication (int n)
+{
+int count  = 1;
+int result = 1;
+while (count<=n)
+{
+result = result * count;
+count++;
+}
+Console.WriteLine( $"{result}");
+}
+Console.WriteLine("Введите число N :");
+int number  = int.Parse(Console.ReadLine());
+Multiplication(number);
