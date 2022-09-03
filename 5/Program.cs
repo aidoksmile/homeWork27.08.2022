5b3282cf3a1837e0f9b905e71e5701ea97eadab0
Aida <Aida@MacBook-Air.local>
2022-09-03 10:08:46 +0600

    Показать кубы чисел, заканчивающихся на четную цифру

---
+++ 5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+int N = new Random().Next(1, 10);
+Console.WriteLine($"N = {N}");
+int number = 1;
+Console.WriteLine("Кубы чисел, заканчивающихся на четную цифру:");
+while (number <= N)
+{
+    int cube = number * number * number;
+    if (cube % 2 == 0) 
+    Console.Write(cube + ", ");
+    number++;
+}
+Console.WriteLine();
